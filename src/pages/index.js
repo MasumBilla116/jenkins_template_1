460b13939b981ee61a14e6ab83f067dc823c8ee1
MasumBilla116 <masumbillacse@gmail.com>
2024-05-27 14:37:17 +0600

    remove name

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By {" "}
+            By Masum Billa{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
